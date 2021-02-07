c7380bc3a138ea7e0eea9e8a63169970477603ed
ibrown8 <43243853+ibrown8@users.noreply.github.com>
2021-02-07 15:19:08 -0600

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -3,11 +3,11 @@ import os
 #This is a rough draft of the user interface code, I will make some edits later!
 #Please give me feed back especially if some of the text could be worded better
 def enter_date():
-    layout = [[gui.Text("Enter the date")], [gui.Input(key="Input")], [gui.Button("Continue")]]
+    layout = [[gui.Text("Enter the date")], [gui.Input(key="Input")], [gui.OK()]]
     window = gui.Window("Title", layout)
     while True:
         event, values = window.read()
-        if event == gui.WIN_CLOSED or event == "Continue":
+        if event == gui.WIN_CLOSED or event == "OK":
             break
     window.close()
     return values["Input"]
@@ -15,7 +15,8 @@ def enter_info():
     layout = [[gui.Text("* means that this field is required")], [gui.Text("Input File *"), gui.Input(key="Infile")], [gui.Text("Output File"), gui.Input(key="Ouput")],
               [gui.Text("File Type"), gui.Input(key="Filetype")], [gui.Text("Output Format"), gui.Input(key="Outputformat")], 
               [gui.Text("Latitude"), gui.Input(key="Lat")], [gui.Text("Longitude"), gui.Input(key="Long")], [gui.Text("Week"), gui.Input(key="Week")],
-              [gui.Text("Overlap"), gui.Input(key="Overlap")], [gui.Text("SPP"), gui.Input(key="SPP")], [gui.Text("Sensitivity"), gui.Input(key="Sensitivity")], [gui.Button("OK")]]
+              [gui.Text("Overlap"), gui.Input(key="Overlap")], [gui.Text("SPP"), gui.Input(key="SPP")], [gui.Text("Sensitivity"), gui.Input(key="Sensitivity")], 
+              [gui.OK()]]
     window = gui.Window("Title", layout)
     while True:
         event, values = window.read()
@@ -23,7 +24,7 @@ def enter_info():
             break
     window.close()
     if values["Infile"] == None:
-        window2 = gui.Window("Error Message", [[gui.Text("You need to specify which file or directory to analyse")]], [[gui.Button("OK")]])
+        window2 = gui.Window("Error Message", [[gui.Text("You need to specify which file or directory to analyse")]], [[gui.OK()]])
         #To do: Return to the beginning of this function
         #To do: Check if the input file is valid
     #This builds the command line argument piece by piece
@@ -53,21 +54,21 @@ def enter_info():
     if values["Week"]:
         temp2 = int(values["Week"])
         if temp2 < 1 or temp2 > 48:
-            window2 =  gui.Window("Error Message", [[gui.Text(range_error.format("Week", "Integer", 1, 48))]], [[gui.Button("OK")]])
+            window2 =  gui.Window("Error Message", [[gui.Text(range_error.format("Week", "Integer", 1, 48))]], [[gui.OK()]])
             #To do: Return to the beginning of this function or special function to handel error
         else:
             command += "--week {} ".format(temp2)
     if values["Overlap"]:
         temp3 = float(values["Overlap"])
         if temp3 < 0.0 or temp3 > 2.9:
-            window2 =  gui.Window("Error Message", [[gui.Text(range_error.format("Overlap", "Decimal", 0.0, 2.9))]], [[gui.Button("OK")]])
+            window2 =  gui.Window("Error Message", [[gui.Text(range_error.format("Overlap", "Decimal", 0.0, 2.9))]], [[gui.OK()]])
             #To do: Return to the beginning of this function or special function to handel error
         else:
             command += "--overlap {} ".format(temp3)
     if values["Sensitivity"]:
         temp4 = float(values["Sensitivity"])
         if temp4 < 0.25 or temp4 > 2.0:
-            window2 =  gui.Window("Error Message", [[gui.Text(range_error.format("Sensitivity", "Decimal", 0.25, 2.0))]], [[gui.Button("OK")]])
+            window2 =  gui.Window("Error Message", [[gui.Text(range_error.format("Sensitivity", "Decimal", 0.25, 2.0))]], [[gui.OK()]])
             #To do: Return to the beginning of this function or special function to handel error
         else:
             command += "--sensitivity {} "
