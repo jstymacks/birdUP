9daff5bc4a7158c8afaf5d46f2ce5bc2a1800e4f
ibrown8 <43243853+ibrown8@users.noreply.github.com>
2021-02-15 12:04:16 -0600

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -8,21 +8,34 @@ formats
 supported_langs = ["English"]
 layout_dictionary = dict()
 window_size = (800, 480)
+class Settings():
+    def __init__():
+        self.lat = 41.9
+        self.long = -87.6
+        self.language = "English"
+        self.result_format = "raven"
+        self.overlap = 1.0
+        self.sensitivity = 1.0
+        self.min_conf = 0.1
+device_settings = Settings()
 layout_dictionary["opening"] =  [   [gui.Button("Start")], [gui.Button("Settings")], [gui.Button("Continue")]   ]
 layout_dictionary["settings"] = [   [gui.Button("Location"), gui.Button("Scheduling")], [gui.Button("Parameters"), gui.Button("Output")],
                                     [gui.Button("Language"), gui.Button("Network")], [gui.Button("Power"), gui.Button("Back")]  ]
 layout_dictionary["location"] = [   [gui.Text("Latitude: from 90°S to 90°N")],
-                                    [gui.Slider(key = "Lat", range = (-90,90),  default_value = 42, size=(20,15), orientation='horizontal', font=("Helvetica", 12))],
+                                    [gui.Slider(key = "Lat", range = (-90,90, 0.1),  default_value = device_settings.lat, size=(20,15), orientation='horizontal', font=("Helvetica", 12))],
                                     [gui.Text("Longitude: from 180°W to 180°E")],
-                                    [gui.Slider(key = "Long", range = (-180, 180), default_value = -88, size=(20,15), orientation='horizontal', font=("Helvetica", 12))],
+                                    [gui.Slider(key = "Long", range = (-180, 180, 0.1), device_settings = device_settings.long, size=(20,15), orientation='horizontal', font=("Helvetica", 12))],
                                     [gui.OK()]  ]
 layout_dictionary["parameters"] =[  [gui.Text("Overlap")],
-                                    [gui.Slider(key = "Overlap", range(0, 2.9, 0.1), default_value = 1, size=(20,15), orientation='horizontal', font=("Helvetica", 12))],
+                                    [gui.Slider(key = "Overlap", range(0, 2.9, 0.1), default_value = device_settings.overlap, size=(20,15), orientation='horizontal', font=("Helvetica", 12))],
                                     [gui.SPP("Sensitivity")],
-                                    [gui.Slider(key = "Sensitivity", range(0.25, 2.0, 0.05), default_value = 1, size=(20,15), orientation='horizontal', font=("Helvetica", 12))],
+                                    [gui.Slider(key = "Sensitivity", range(0.25, 2.0, 0.05), default_value = device_settings.sensitivity, size=(20,15), orientation='horizontal', font=("Helvetica", 12))],
+                                    [gui.Text("Minumum Confidence")],
+                                    [gui.Slider(key = "Min_Conf", range(0.01, 0.99, 0.01), default_value = device_settings.min_conf, size=(20,15), orientation='horizontal', font=("Helvetica", 12))]
                                     [gui.OK()] ]
 layout_dictionary["format"] =   [   [gui.Spin(key="Format", values = ["Raven", "Audacity"])], [gui.OK()]]
 
+command_base = "python analyze.py --i {} --results {} --lat {} --lon {} --overlap {} --sensitivity {} --min_conf {}"
 def enter_date():
     year_layout = [gui.Text("Year", size = (6,1)), gui.Spin(key = "Year", values = ["2021", "2022", "2023", "2024"])]
     month_layout = [gui.Text("Month", size = (6,1)), gui.Spin(key = "Month", values = months)] 
@@ -35,20 +48,32 @@ def enter_date():
     window.close()
     return values["Year"], values["Month"]
 def enter_lat_long():
-    window = gui.Window("Enter the Latitude and Longitude", layout_dictionary["location"])
+    window = gui.Window("Enter the Latitude and Longitude", layout_dictionary["location"], window_size)
     while True:
         event, values = window.read()
-        print(event, values)
         if event == gui.WIN_CLOSED or event == "OK":
             window.close()
-            return values["Lat"], values["Long"]
-            #To do: Return to the beginning of this function
+            break
+    if values != None:
+        device_settings.lat = values["Lat"]
+        device_settings.long = values["Long"]
+def enter_parameters():
+    window = gui.Window("Parameters", layout_dictionary["parameters"], window_size)
+    while True:
+        event, values = window.read()
+        if event == gui.WIN_CLOSED or event == "OK":
+            window.close()
+            break
+    if values != None:
+        device_settings.overlap = values["Overlap"]
+        device_settings.sensitivity = values["Sensitivity"]
+        device_settings.min_conf = values["Min_Conf"]
 def enter_info():
     layout = [[gui.Text("* means that this field is required")], [gui.Text("Input File *", size=(14,1)), gui.Input(key="Infile")], 
               [gui.Text("Output File", size=(14,1)), gui.Input(key="Ouput")], [gui.Text("File Type", size=(14,1)), gui.Input(key="Filetype")], 
               [gui.Text("Output Format", size=(14,1)), gui.Input(key="Outputformat")], 
-              [gui.Text("Week", size=(14,1)), gui.Input(key="Week")], [gui.Text("Overlap", size=(14,1)), gui.Input(key="Overlap")], 
-              [gui.Text("SPP", size=(14,1)), gui.Input(key="SPP")], [gui.Text("Sensitivity", size=(14,1)), gui.Input(key="Sensitivity")], 
+              [gui.Text("Week", size=(14,1)), gui.Input(key="Week")],
+              [gui.Text("SPP", size=(14,1)), gui.Input(key="SPP")],  
               [gui.OK()]]
     window = gui.Window("Title", layout)
     while True:
@@ -116,4 +141,3 @@ def start_command(filename):
                 break
         window.close()
         return 0
-
