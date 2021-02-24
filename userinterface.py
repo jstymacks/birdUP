e8956e98fbd12724032aa3972fc7bfd1c4379a0b
ibrown8 <43243853+ibrown8@users.noreply.github.com>
2021-02-23 20:52:44 -0600

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -5,7 +5,6 @@ from datetime import date
 #Please give me feed back especially if some of the text could be worded better
 months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
 supported_langs = ["English"]
-layout_dictionary = dict()
 window_size = (800, 480)
 class Settings():
     def __init__(self):
@@ -18,10 +17,6 @@ class Settings():
         self.min_conf = 0.1
 device_settings = Settings()
 sg.theme("DarkGreen")
-layout_dictionary["opening"] =  [   [sg.Button("Start")], [sg.Button("Settings")], [sg.Button("Continue")]   ]
-layout_dictionary["settings"] = [   [sg.Button("Location"), sg.Button("Scheduling")], [sg.Button("Parameters"), sg.Button("Output")],
-                                    [sg.Button("Language"), sg.Button("Network")], [sg.Button("Power"), sg.Button("Back")]  ]
-layout_dictionary["format"] =   [   [sg.Spin(key="Format", values = ["Raven", "Audacity"])], [sg.OK()]]
 command_base = "python analyze.py --i {} --results {} --lat {} --lon {} --overlap {} --sensitivity {} --min_conf {}"
 def enter_date():
     year_layout = [sg.Text("Year", size = (6,1)), sg.Spin(key = "Year", values = ["2021", "2022", "2023", "2024"])]
@@ -68,6 +63,14 @@ def enter_parameters():
         device_settings.overlap = values["Overlap"]/10
         device_settings.sensitivity = values["Sensitivity"]/100
         device_settings.min_conf = values["Min_Conf"]/100
+def output_format():
+    layout = [   [sg.Spin(key="Format", values = ["raven", "audacity"])], [sg.OK()]]
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "OK":
+            window.close()
+            break
+    device_settings.result_format = values["Format"]
 def settings_menu():
     button_size = (12, 1)
     layout = [  [sg.Button("Location", size = button_size), sg.Button("Scheduling", size = button_size)], [sg.Button("Parameters", size = button_size), sg.Button("Output", size = button_size)],
@@ -79,10 +82,10 @@ def settings_menu():
             break
         if event == "Location":
             enter_lat_long()
-            break
         if event == "Parameters":
             enter_parameters()
-            break
+        if event == "Output":
+            output_format()
     window.close()
     return values
 layout_dictionary["location"] = enter_lat_long
@@ -107,13 +110,6 @@ def enter_info():
     #This builds the command line argument piece by piece
     range_error = "{} should be a {} between {} and {}"
     command = "python analyze.py --i {} ".format(values["Infile"])
-    if values["Outputformat"]:
-        temp1 = values["Outputformat"]
-        if temp1 != "audacity" and temp1 != "raven":
-            sg.popup("Error", "Output format should be raven or audacity")
-            #To do: Return to the beginning of this function or special function to handel error
-        else:
-            command += "--result {} ".format(temp1)
     if values["Week"]:
         temp2 = int(values["Week"])
         if temp2 < 1 or temp2 > 48:
@@ -121,38 +117,18 @@ def enter_info():
             #To do: Return to the beginning of this function or special function to handel error
         else:
             command += "--week {} ".format(temp2)
-    if values["Overlap"]:
-        temp3 = float(values["Overlap"])
-        if temp3 < 0.0 or temp3 > 2.9:
-            window2 =  sg.Window("Error Message", [[sg.Text(range_error.format("Overlap", "Decimal", 0.0, 2.9))]], [[sg.OK()]])
-            #To do: Return to the beginning of this function or special function to handel error
-        else:
-            command += "--overlap {} ".format(temp3)
-    if values["Sensitivity"]:
-        temp4 = float(values["Sensitivity"])
-        if temp4 < 0.25 or temp4 > 2.0:
-            window2 =  sg.Window("Error Message", [[sg.Text(range_error.format("Sensitivity", "Decimal", 0.25, 2.0))]], [[sg.OK()]])
-            #To do: Return to the beginning of this function or special function to handel error
-        else:
-            command += "--sensitivity {} "
     return command
 #print(enter_date())
 #print(date.today())
 #enter_lat_long()
 #layout_dictionary["location"]()
 #enter_parameters()
-settings_menu()
-filename = input("Enter the Filename")
-command = command_base.format(filename, device_settings.result_format, device_settings.lat, device_settings.long, device_settings.overlap, device_settings.sensitivity, device_settings.min_conf)
-print(command)
-enter_parameters()
-command = command_base.format(filename, device_settings.result_format, device_settings.lat, device_settings.long, device_settings.overlap, device_settings.sensitivity, device_settings.min_conf)
-print(command)
-def start_command(filename):
-    cmd = command_base.format(filename, device_settings.result_format, device_settings.lat, device_settings.long, device_settingss.overlap, device_settings.sensitivity, device_settings.min_conf)
+def run_command(filename):
+    cmd = command_base.format(filename, device_settings.result_format, device_settings.lat, device_settings.long, device_settings.overlap, device_settings.sensitivity, device_settings.min_conf)
+    print(cmd)
     failure = os.system(cmd)
     if failure:
-        window = sg.Window("Error Window", [[sg.Text("There was an error")]], [sg.Button("OK")])
+        window = sg.Window("Error Window", [[sg.Text("There was an error")], [sg.Button("OK")]], size = window_size)
         while True:
             event, values = window.read()
             if event == sg.WIN_CLOSED or event == "OK":
@@ -160,10 +136,26 @@ def start_command(filename):
         window.close()
         return -1
     else:
-        window = sg.Window("Success Window", [[sg.Text("The analysis was succesful")]], [sg.Button("OK")])
+        window = sg.Window("Success Window", [[sg.Text("The analysis was succesful")], [sg.Button("OK")]], size = window_size)
         while True:
             event, values = window.read()
             if event == sg.WIN_CLOSED or event == "OK":
                 break
         window.close()
         return 0
+def main():
+    layout = [  [sg.Button("Start", size = (12, 1))], [sg.Button("Settings", size = (12, 1))], [sg.Button("Documentation", size = (12, 1))]]
+    window = sg.Window("Opening Screen", layout, size = window_size)
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        if event == "Settings":
+            settings_menu()
+        if event == "Start":
+            break
+    filename = input("Enter the Filename")    
+    run_command(filename)
+if __name__ == "__main__":
+    main()
+
